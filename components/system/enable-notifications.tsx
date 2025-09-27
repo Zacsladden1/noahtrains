8dde03ae6f8ab9a91d9108896535c2df2f4a293c
Zac Sladden <zacsladden@mac.home>
2025-09-27 14:34:58 +0100

    push

--- components/system/enable-notifications.tsx
+++ components/system/enable-notifications.tsx
@@ -43,7 +43,7 @@ export default function EnableNotificationsButton({ className, onRegistered }: P
         userVisibleOnly: true,
         applicationServerKey: (() => {
           const base64 = publicKey || '';
-          const padding = '='.repeat((4 - (base4.length % 4)) % 4);
+          const padding = '='.repeat((4 - (base64.length % 4)) % 4);
           const b64 = (base64 + padding).replace(/-/g, '+').replace(/_/g, '/');
           const raw = atob(b64);
           const arr = new Uint8Array(raw.length);
